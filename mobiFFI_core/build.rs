57d9e3ce4a841a3ca9a9b3cf29c3debea3e82e8d
Ali <ali94_200970@yahoo.com>
2025-06-14 17:39:00 +0200

    cbindgen config and build script

--- mobiFFI_core/build.rs
+++ mobiFFI_core/build.rs
@@ -1,3 +1,12 @@
 fn main() {
+    let crate_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
+    
+    cbindgen::Builder::new()
+        .with_crate(&crate_dir)
+        .with_config(cbindgen::Config::from_root_or_default(&crate_dir))
+        .generate()
+        .expect("Unable to generate bindings")
+        .write_to_file("bindings.h");
+    
     println!("cargo:rerun-if-changed=src/");
 }
