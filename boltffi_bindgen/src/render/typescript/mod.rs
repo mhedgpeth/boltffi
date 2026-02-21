4ae99a1b64b1770565910fdba57e1841a5b451fa
Ali A. Hilal <ali94_200970@yahoo.com>
2026-02-21 22:22:05 +0100

    pass experimental features to TS

--- boltffi_bindgen/src/render/typescript/mod.rs
+++ boltffi_bindgen/src/render/typescript/mod.rs
@@ -4,6 +4,6 @@ mod plan;
 mod templates;
 
 pub use emit::*;
-pub use lower::TypeScriptLowerer;
+pub use lower::{TypeScriptExperimental, TypeScriptLowerer};
 pub use plan::*;
 pub use templates::TypeScriptEmitter;
